8a42189153e2a4361304e1d72890a4eec9c124fc
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-05-26 19:46:33 +0100

    Add some definitions in player class

--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,22 @@
 #include "player.hpp"
+
+Player::Player()
+{
+    //texture = LoadTexture("");
+    position.x = 100;
+    position.y = 100;
+}
+
+Player::~Player()
+{
+    //UnloadTexture(texture);
+}
+
+void Player::Draw()
+{
+    // placeholder rectangle
+    Vector2 size;
+    size.x = 20;
+    size.y = 20;
+    DrawRectangleV(position, size, RED);
+}
