ab916ae3c1b1633998c20b9730c927d22fb8b8eb
Cameron.McKinney.Prog <cameron.mckinney.prog@gmail.com>
2024-05-26 19:12:35 +0100

    Add basic declarations in player class

--- src/player.hpp
+++ src/player.hpp
@@ -1,5 +1,18 @@
 #pragma once
+#include <raylib.h>
 
 class Player
 {
+public:
+    Player();
+    ~Player();
+    void Draw();
+    void Thrust();
+    void RotateLeft();
+    void RotateRight();
+    void FireWeapon();
+
+private:
+    Texture2D texture;
+    Vector2 position;
 };
